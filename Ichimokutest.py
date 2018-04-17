6a59e72375f8cbb6eb2a89b4752384ee2b7a2e06
Maxim <maxim.gospodinko@gmail.com>
2018-04-17 21:58:34 +0300

    ichimoku lagspan

---
+++ Ichimokutest.py
@@ -0,0 +1,27 @@
+#Tenkan-sen (Conversion Line): (9-period high + 9-period low)/2))
+
+def tenkan(df, period):
+    
+    tenkan = []
+    highs = df['H']
+    lows = df['L']
+    for i in range (len (highs)):
+        if i < period:
+            tenkan.append(None)
+        else:
+            tempHighs = highs [i - period:i]
+            tempLows = lows [i - period:i]
+            averageHighs = sum (highs) / period
+            averageLows = sum (lows) / period
+            tenkan.append ((averageHighs + averageLows)/2)
+    return tenkan
+
+#Kijun-sen (Base Line): (26-period high + 26-period low)/2))
+#Senkou Span A (Leading Span A): (Conversion Line + Base Line)/2))
+#Senkou Span B (Leading Span B): (52-period high + 52-period low)/2))
+#Close plotted 26 days in the past
+
+
+
+#calculating lagging span - shift pandas dataframe with closing prices by period
+lagSpan = s = closes.shift(-100)
--- SMAEMAtest.py
+++ SMAEMAtest.py
@@ -3,40 +3,60 @@ from matplotlib import style
 import pandas as pd
 import numpy as np
 
-
-
 df = pd.read_json('candles.json')
-df = df['C']
+closes = df['C']
 
-
-def SMA(candles, period):
+def SMA(closes, period):
     
     SMA = []
-    for i in range (len (candles)):
+    for i in range (len (closes)):
         if i < period:
             SMA.append(None)
         else:
-            tempList = candles [i - period:i]
+            tempList = closes [i - period:i]
             average = sum (tempList) / period
             SMA.append (round (average, 2))
     return SMA
 
-
-def EMA (candles, period):
+def EMA (closes, period):
     
     EMA = []
-    initSMA = (sum (candles [:period])) / period
+    initSMA = (sum (closes [:period])) / period
     multiplier = 2/(period+1)
     EMA.append (initSMA)
-    for i in range (len (candles)):
-        EMA.append ((candles [i] - EMA [i]) * multiplier + EMA [i])
+    for i in range (len (closes)):
+        EMA.append ((closes [i] - EMA [i]) * multiplier + EMA [i])
     return EMA
 
-ema = EMA(df, 50)
-sma = SMA(df, 50)
+def tenkan(df, period):
+    
+    tenkan = []
+    highs = df['H']
+    lows = df['L']
+    for i in range (len (highs)):
+        if i < period:
+            tenkan.append(None)
+        else:
+            tempHighs = highs [i - period:i]
+            tempLows = lows [i - period:i]
+            averageHighs = sum (highs) / period
+            averageLows = sum (lows) / period
+            tenkan.append ((averageHighs + averageLows)/2)
+    return tenkan
 
+ema = EMA(closes, 50)
+sma = SMA(closes, 50)
+tenkan = tenkan(df, 9)
+#span here has arbitrary 26 period
+period = 26
+lagSpan = closes.shift(-period)
 
-plt.plot(df)
+#PLOT ALL THE STUFF!!!
+plt.plot(lagSpan)
+plt.plot(closes)
 plt.plot(sma)
 plt.plot(ema)
+plt.plot(tenkan)
 plt.show()
+
+print(tenkan)
